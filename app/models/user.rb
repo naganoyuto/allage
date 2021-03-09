995641afb63ac09fcaba8de93d8412101bb3531a
singori <naganoyuto@icloud.com>
2021-03-10 08:52:50 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :nickname
+    validates :height, numericality: {only_integer: greater_than: 1, less_than: 999, message: 'には3桁以内かつ半角数字で入力してください'}
+  end
 end
