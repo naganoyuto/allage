62d22112544e55bdf351ebce260a3e9afb5242a3
singori <naganoyuto@icloud.com>
2021-03-17 14:42:18 +0900

    post,tagのモデルとコントローラーの設定

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
-  has_many :post_tags, dependent: :destroy, forign_key: 'tag_id'
-  has_many :posts, through: :tag_maps
+  has_many :post_tags, dependent: :destroy, foreign_key: 'tag_id'
+  has_many :posts, through: :post_tags
 end
